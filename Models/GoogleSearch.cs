4efb8ec7a616d4a74d46752c57e0d375bb7dfc7a
SHADI JAM <shadijam@SHADIs-MacBook-Pro.local>
2016-10-26 08:49:38 -0500

    adding models

---
+++ Models/GoogleSearch.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+// using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+
+public class Location
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
+public class Geometry
+{
+    public Location location { get; set; }
+}
+public class Result
+{
+    public Geometry geometry { get; set; }
+}
+public class SearchRO
+{
+    public List<Result> results { get; set; }
+}
+
+
+
